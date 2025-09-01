f4b4547a6b5c5487bb36a1d328bca18f58ef2656
KHKAC <rudgnsrb888@naver.com>
2025-09-01 15:49:49 +0900

    250901 젤라티노 인풋 시스템

--- Assets/00.Scripts/PlayerController.cs
+++ Assets/00.Scripts/PlayerController.cs
@@ -1,19 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction LeftAction;
     void Start()
     {
-        
+        LeftAction.Enable();
     }
 
     void Update()
     {
+        float horizontal = 0.0f;
+        float vertical = 0.0f;
+        if (LeftAction.IsPressed())
+        {
+            horizontal = -1.0f;
+        }
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+        Debug.Log($"h: {horizontal} / v: {vertical}");
         Vector2 position = transform.position;
-        position.x += 0.1f;
-        position.y -= 0.1f;
+        position.x += 0.1f * horizontal;
+        position.y += 0.1f * vertical;
         transform.position = position;
     }
 }
